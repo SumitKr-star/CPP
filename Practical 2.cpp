9a6767c6808873fedb757cf61c9d59b779f369e3
SumitKr-star <sumitkr781225@gmail.com>
2026-04-15 09:38:48 +0530

    Update Practical 2.cpp

--- Practical 2.cpp
+++ Practical 2.cpp
@@ -1,1 +1,45 @@
-b
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100], n, i, j, k;
+
+    
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    
+    cout << "Enter elements:\n";
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                
+                for (k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--;   
+                j--;  
+            }
+        }
+    }
+
+    
+    cout << "Array after removing duplicates:\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
