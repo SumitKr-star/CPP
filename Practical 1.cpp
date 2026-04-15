ef89f37715833cdfb9f416881bb8353f7945403d
SumitKr-star <sumitkr781225@gmail.com>
2026-04-15 09:36:10 +0530

    Update Practical 1.cpp

--- Practical 1.cpp
+++ Practical 1.cpp
@@ -1,1 +1,31 @@
-n
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    int i;
+    float sum = 0;
+
+    // Check if value is given in command line
+    if (argc > 1)
+    {
+        n = atoi(argv[1]);
+    }
+    else
+    {
+        printf("Enter value of n: ");
+        scanf("%d", &n);
+    }
+
+    // Calculate the sum
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + (float)i / (i + 4);
+    }
+
+    // Print the result
+    printf("Sum = %f", sum);
+
+    return 0;
+}
