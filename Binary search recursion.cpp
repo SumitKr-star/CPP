0c5dabd79b95d5e86c84fb11aaa8c97b2070d833
SumitKr-star <sumitkr781225@gmail.com>
2026-05-11 14:22:49 +0530

    Update Binary search recursion.cpp

--- Binary search recursion.cpp
+++ Binary search recursion.cpp
@@ -1,1 +1,44 @@
-h
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    if(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if(key < arr[mid])
+        {
+            return binarySearch(arr, low, mid - 1, key);
+        }
+
+        return binarySearch(arr, mid + 1, high, key);
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int key = 30;
+
+    int result = binarySearch(arr, 0, n - 1, key);
+
+    if(result != -1)
+    {
+        cout << "Element found at position " << result;
+    }
+    else
+    {
+        cout << "Element not found";
+    }
+
+    return 0;
+}
