1090e485dfce902fa18cc1cde97a834e8541fcb2
SumitKr-star <sumitkr781225@gmail.com>
2026-05-11 14:32:19 +0530

    Update Matrix Transpose.cpp

--- Matrix Transpose.cpp
+++ Matrix Transpose.cpp
@@ -1,1 +1,33 @@
-z
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10][10], transpose[10][10];
+    int r, c;
+
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    cout << "Enter matrix:\n";
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    cout << "Transpose of matrix:\n";
+    for(int i = 0; i < c; i++) {
+        for(int j = 0; j < r; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
