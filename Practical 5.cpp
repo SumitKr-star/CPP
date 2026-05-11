416fc066bdb111de9010e6f569654e2c3b7ffe89
SumitKr-star <sumitkr781225@gmail.com>
2026-05-11 14:13:30 +0530

    Update Practical 5.cpp

--- Practical 5.cpp
+++ Practical 5.cpp
@@ -1,1 +1,33 @@
-5.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {1, 2, 3};
+    int b[] = {10, 20, 30};
+
+    int al = sizeof(a) / sizeof(a[0]);
+    int bl = sizeof(b) / sizeof(b[0]);
+
+    int c[10];  
+
+    
+    for(int i = 0; i < al; i++)
+    {
+        c[i] = a[i];
+    }
+
+    
+    for(int i = 0; i < bl; i++)
+    {
+        c[al + i] = b[i];
+    }
+
+    
+    for(int i = 0; i < al + bl; i++)
+    {
+        cout << c[i] << " ";
+    }
+
+    return 0;
+}
